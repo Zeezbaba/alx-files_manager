b5f176767c83478d55c65de065d89ecfb6101e1f
Zeezbaba <okelejitimi93@gmail.com>
2024-08-04 00:40:48 -0700

    first-API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -4,14 +4,14 @@ const dbClient = require('../utils/db');
 class AppController {
   static async getStatus(req, res) {
     if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.status(200).json({ "redis": true, "db": true });
+      res.status(200).json({ redis: true, db: true });
     }
   }
 
   static async getStats(req, res) {
     const numUser = await dbClient.nbUsers();
     const numFile = await dbClient.nbFiles();
-    res.status(200).json({ "users": numUser, "files": numFile });
+    res.status(200).json({ users: numUser, files: numFile });
   }
 }
 
--- utils/db.js
+++ utils/db.js
@@ -6,7 +6,7 @@ class DBClient {
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'file_manager';
     const url = `mongodb://${host}:${port}`;
-    this.connected = false;
+    // this.connected = false;
 
     this.client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
     this.client.connect()
